7456c54845edd03b5581849e0149ec4b1579f8b2
Erin Nichols <erin.e.nichols@gmail.com>
2011-08-21 18:30:51 -0400

    extracted javascript

---
+++ toggle.js
@@ -0,0 +1,36 @@
+jQuery(function($) {
+  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  
+  $.getJSON('history.json', parseHistory);
+  
+  function parseHistory(history) {
+    var dateLabel = '';
+    var oldDateLabel = 'dummy';
+    var count = 0;
+    for(var i in history) {
+      if(count >= 10) { break; }
+      count++;
+      var dateString = history[i];
+      var date = new Date(dateString * 1000);
+      oldDateLabel = dateLabel;
+      dateLabel = days[date.getDay()] + ', ' + months[date.getMonth()] + ' ' + date.getDate();
+      if(oldDateLabel != dateLabel) {
+        $('<li>', { text: dateLabel, 'data-role': 'list-divider' }).addClass('datapoint').appendTo($('#history'));
+      }
+     
+      $('<li>', { text: date.toLocaleTimeString() }).addClass('datapoint').appendTo($('#history'));
+    }
+    $('#history').listview('refresh');
+  }
+  
+  
+  $('#toggle-button').click(function() {
+    $.getJSON('toggle.php', function(result) {
+      if(result && result.success && result.success == true) {
+        $('.datapoint').remove();
+        parseHistory(result.history);
+      }
+    });
+  });
+});
